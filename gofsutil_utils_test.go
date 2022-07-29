4133abafce6ee73212a10e19b989e9ac352f2dca
Khare <Rajshree.Khare@emc.com>
2022-07-29 16:19:25 +0530

    security enhancements and unit tests added
    Signed-off-by: Rajshree Khare <Rajshree.Khare@dell.com>

---
+++ gofsutil_utils_test.go
@@ -0,0 +1,188 @@
+package gofsutil
+
+import (
+	"errors"
+	"strings"
+	"testing"
+)
+
+func TestValidatePath(t *testing.T) {
+	tests := []struct {
+		path   string
+		result error
+	}{
+		{
+			path:   "/",
+			result: errors.New("Path: / is invalid"),
+		},
+		{
+			path:   "/dev/disk/by-id/wwn-0x60570970000197900046533030394146",
+			result: nil,
+		},
+		{
+			path:   "../../mydevb",
+			result: nil,
+		},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+		t.Run("", func(st *testing.T) {
+			st.Parallel()
+			err := validatePath(tt.path)
+			if err != nil {
+				if tt.result == nil {
+					t.Errorf("Validation of path is incorrect, \n\tgot: %s \n\twant: %v",
+						err, tt.result)
+				} else {
+					if err.Error() != tt.result.Error() {
+						t.Errorf("Validation of path is incorrect, \n\tgot: %s \n\twant: %s",
+							err, tt.result)
+					}
+				}
+			}
+
+		})
+	}
+}
+
+func TestValidateFsType(t *testing.T) {
+	tests := []struct {
+		fsType string
+		result error
+	}{
+		{
+			fsType: "smtp",
+			result: errors.New("FsType: smtp is invalid"),
+		},
+		{
+			fsType: " ",
+			result: errors.New("FsType:   is invalid"),
+		},
+		{
+			fsType: "ext3",
+			result: nil,
+		},
+		{
+			fsType: "ext4",
+			result: nil,
+		},
+		{
+			fsType: "xfs",
+			result: nil,
+		},
+		{
+			fsType: "nfs",
+			result: nil,
+		},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+		t.Run("", func(st *testing.T) {
+			st.Parallel()
+			err := validateFsType(tt.fsType)
+			if err != nil {
+				if tt.result == nil {
+					t.Errorf("Validation of fsType is incorrect, \n\tgot: %s \n\twant: %v",
+						err, tt.result)
+				} else {
+					if err.Error() != tt.result.Error() {
+						t.Errorf("Validation of fsType is incorrect, \n\tgot: %s \n\twant: %s",
+							err, tt.result)
+					}
+				}
+			}
+
+		})
+	}
+}
+
+func TestValidateMountOptions(t *testing.T) {
+	tests := []struct {
+		mountOptions []string
+		result       error
+	}{
+		{
+			mountOptions: []string{"*", "##", "()"},
+			result:       errors.New("Mount option: * is invalid"),
+		},
+		{
+			mountOptions: []string{""},
+			result:       nil,
+		},
+		{
+			mountOptions: []string{"", " ", ""},
+			result:       nil,
+		},
+		{
+			mountOptions: []string{"rw", "noatime"},
+			result:       nil,
+		},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+		t.Run("", func(st *testing.T) {
+			st.Parallel()
+			optsStr := strings.Join(tt.mountOptions, " ")
+			optsStr = strings.TrimSpace(optsStr)
+			if len(optsStr) != 0 {
+				err := validateMountOptions(tt.mountOptions...)
+				if err != nil {
+					if tt.result == nil {
+						t.Errorf("Validation of mountOptions is incorrect, \n\tgot: %s \n\twant: %v",
+							err, tt.result)
+					} else {
+						if err.Error() != tt.result.Error() {
+							t.Errorf("Validation of mountOptions is incorrect, \n\tgot: %s \n\twant: %s",
+								err, tt.result)
+						}
+					}
+				}
+			}
+		})
+	}
+}
+
+func TestValidateMultipathArgs(t *testing.T) {
+	tests := []struct {
+		pathArgs []string
+		result   error
+	}{
+		{
+			pathArgs: []string{"/data0", "-A", "-iR", "/tmp"},
+			result:   nil,
+		},
+		{
+			pathArgs: []string{"-/abc", "-h1", "/dev*"},
+			result:   nil,
+		},
+		{
+			pathArgs: []string{"/"},
+			result:   errors.New("Multipath option: / is invalid"),
+		},
+		{
+			pathArgs: []string{""},
+			result:   nil,
+		},
+	}
+	for _, tt := range tests {
+		tt := tt
+		t.Run("", func(st *testing.T) {
+			st.Parallel()
+			err := validateMultipathArgs(tt.pathArgs...)
+			if err != nil {
+				if tt.result == nil {
+					t.Errorf("Validation of path args is incorrect, \n\tgot: %s \n\twant: %v",
+						err, tt.result)
+				} else {
+					if err.Error() != tt.result.Error() {
+						t.Errorf("Validation of path args is incorrect, \n\tgot: %s \n\twant: %s",
+							err, tt.result)
+					}
+				}
+			}
+		})
+	}
+}
