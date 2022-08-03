2211d161b95e728fc4ffd5626f3a84204408c76e
Rajshree Khare <Rajshree.Khare@dell.com>
2022-08-03 15:48:42 +0530

    regex sample added

--- gofsutil_utils.go
+++ gofsutil_utils.go
@@ -25,6 +25,7 @@ func validateFsType(fsType string) error {
 
 func validateMountOptions(mountOptions ...string) error {
 	for _, opt := range mountOptions {
+		// regex e.g: "rw", "noatime", "", " "
 		matched, err := regexp.Match(`[\w]+[=]*[\w]*`, []byte(opt))
 		if !matched || err != nil {
 			return errors.New("Mount option: " + opt + " is invalid")
@@ -36,12 +37,14 @@ func validateMountOptions(mountOptions ...string) error {
 func validateMultipathArgs(options ...string) error {
 	for _, opt := range options {
 		// check for options
+		// regex e.g: "-A", "-iR", "-h1", "-/data0", "", " "
 		matched, err := regexp.Match(`[[-][AaBbCcdFfhilpqrTtUuWw0-9]+]*[0-9]*`, []byte(opt))
 		if matched && err == nil {
 			continue
 		}
 
 		// check for file or device path
+		// regex e.g: "/tmp", "/data0", "", " "
 		if err := validatePath(filepath.Clean(opt)); err != nil {
 			return errors.New("Multipath option: " + opt + " is invalid")
 		}
